89eba48f9481415d39586bad9c33a937ab8e0376
Frank Wei <frank@frankwei.xyz>
2021-06-08 15:10:38 +0800

    fix: rename app

--- src/constant/EIP712Domain.ts
+++ src/constant/EIP712Domain.ts
@@ -1,6 +1,6 @@
 export const getEIP712Profile = (chainId: number) => ({
-    name: 'Ether Mail',
-    version: '1',
-    chainId,
-    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
-})
+  name: "Hodl To Unlock",
+  version: "1",
+  chainId,
+  verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+});
