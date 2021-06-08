093b06c81b1420ab1d29ff5cded340125136d720
Frank Wei <frank@frankwei.xyz>
2021-06-08 15:27:51 +0800

    feat: good to fetch and decrypt

---
+++ src/pages/api/[hash]/index.ts
@@ -0,0 +1,91 @@
+import { recoverTypedSignature } from "eth-sig-util";
+import { utils } from "ethers";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getEIP712Profile } from "../../../constant/EIP712Domain";
+import { WorkerKV } from "../../../constant/kvclient";
+import { decrypt } from "../../../encryption";
+import { EncryptedSnippet, Snippet } from "../../../typing";
+import { ipfsCat } from "../../../utils/ipfs";
+
+function recoverFromSig(
+  chainId: number,
+  hash: string,
+  token: string,
+  sig: string
+) {
+  const recoveredWallet = recoverTypedSignature({
+    data: {
+      types: {
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+        ],
+        RequestUnlock: [
+          { name: "token", type: "string" },
+          { name: "hash", type: "string" },
+        ],
+      },
+      primaryType: "RequestUnlock",
+      domain: getEIP712Profile(chainId),
+      message: {
+        token,
+        hash,
+      },
+    },
+    sig,
+  });
+  return utils.getAddress(recoveredWallet);
+}
+
+async function getPreviewOf(hash: string, res: NextApiResponse<any>) {
+  return getUnlockedContent(hash, "", 0, "", res);
+}
+
+type UnlockedSnippet = Snippet & { owner: string };
+
+async function getUnlockedContent(
+  hash: string,
+  token: string,
+  chainId: number,
+  sig: string,
+  res: NextApiResponse<UnlockedSnippet | { message: string }>
+) {
+  // const recovered = recoverFromSig(chainId, hash, sig)
+  const encryptedData = await ipfsCat<EncryptedSnippet>(hash);
+  if (!encryptedData.content || !encryptedData.iv) {
+    return res.status(400).json({ message: "Bad file for us" });
+  }
+  // trying to find the encryption key
+  const key = await WorkerKV.getSecretKey(hash);
+  if (!key)
+    return res.status(400).json({ message: "Encryption Key was removed" });
+
+  // Decrypt with the key
+  const result = decrypt(encryptedData, key);
+  // trying to parse to Snippet
+  const decryptedSnippet = JSON.parse(result) as Snippet;
+
+  // produce the result
+  return res.status(200).json({
+    ...decryptedSnippet,
+    owner: encryptedData.owner,
+  });
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { hash } = req.query as { hash: string };
+  const { sig, chainId, token } = req.body;
+
+  switch (req.method) {
+    case "GET":
+      return getPreviewOf(hash, res);
+    case "POST":
+      return getUnlockedContent(hash, token, Number(chainId), sig, res);
+    default:
+      return res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
+
+export default handler;
--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -2,21 +2,29 @@ import axios from "axios";
 import FormData from "form-data";
 
 export async function uploadToPublic(file: any) {
-    console.info('uploadToPublic start')
-    const fd = new FormData();
-    fd.append('file', file);
-    const { data } = await axios.post<{
-        "Name": string;
-        "Hash": string;
-        "Size": string;
-    }>(
-        'https://ipfs.infura.io:5001/api/v0/add',
-        fd,
-        {
-            headers: fd.getHeaders(),
-            timeout: 1000 * 15,
-            params: { pin: 'true' },
-        }
-    );
-    return data.Hash
+  console.info("uploadToPublic start");
+  const fd = new FormData();
+  fd.append("file", file);
+  const { data } = await axios.post<{
+    Name: string;
+    Hash: string;
+    Size: string;
+  }>("https://ipfs.infura.io:5001/api/v0/add", fd, {
+    headers: fd.getHeaders(),
+    timeout: 1000 * 15,
+    params: { pin: "true" },
+  });
+  return data.Hash;
+}
+
+export async function ipfsCat<T>(hash: string) {
+  const { data } = await axios.post<T>(
+    "https://ipfs.infura.io:5001/api/v0/cat",
+    undefined,
+    {
+      timeout: 1000 * 15,
+      params: { arg: hash },
+    }
+  );
+  return data;
 }
