7232d5df26a0c5a8df4d3118e904eac60abf1504
Frank Wei <frank@frankwei.xyz>
2021-06-08 17:44:46 +0800

    basic layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.css'
 import Providers from "./_provider";
+import styled from "styled-components";
 import type { AppProps } from 'next/app'
+
+const CenteredPage = styled.div`
+    width: 800px;
+    min-height: 100vh;
+    margin: 0 auto;
+    max-width: 88vw;
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Providers>
-    <Component {...pageProps} />
+    <CenteredPage>
+        <Component {...pageProps} />
+    </CenteredPage>
   </Providers>
 }
 export default MyApp
