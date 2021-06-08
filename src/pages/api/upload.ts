687f57b8b988725c6a18b09548c85c40339f26b3
Frank Wei <frank@frankwei.xyz>
2021-06-08 14:56:20 +0800

    feat: write secret key when upload

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,5 +1,6 @@
 import { utils } from "ethers"
 import { NextApiRequest, NextApiResponse } from "next"
+import { WorkerKV } from "../../constant/kvclient"
 import { encrypt, getRandomSecretKey } from "../../encryption"
 import { EncryptedSnippet, Snippet } from "../../typing"
 import { uploadToPublic } from "../../utils/ipfs"
@@ -21,6 +22,7 @@ async function upload(req: NextApiRequest, res: NextApiResponse<UploadReturn>) {
     } as Snippet), privateKey)
     encrypted.owner = checksumedAuthorWallet;
     const hash = await uploadToPublic(JSON.stringify(encrypted))
+    await WorkerKV.setSecretKey(hash, privateKey);
     return res.status(201).json({ encrypted, hash, privateKey })
 }
 
