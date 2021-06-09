86b8c134102e20a7396b4e3e0d96d60f631014e6
Frank Wei <frank@frankwei.xyz>
2021-06-09 14:24:25 +0800

    style: better UI & can have title

--- src/components/CreateSnippet.tsx
+++ src/components/CreateSnippet.tsx
@@ -1,20 +1,26 @@
-import React, { useCallback, useMemo, useRef } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import axios from "axios";
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { UploadReturn } from "../typing";
 import { useWallet } from "use-wallet";
-import { Button } from "@geist-ui/react";
+import { Button, Input } from "@geist-ui/react";
 import { useBoolean } from "ahooks";
+import styled from "styled-components";
 
 type CreateSnippetParams = {
     // callback
     onSent: (obj: UploadReturn) => void
 }
 
+const CreateSnippetContainer = styled.div`
+max-width: 100%;
+`
+
 export default function CreateSnippet({ onSent }: CreateSnippetParams) {
     const wallet = useWallet()
+    const [title, setTitle] = useState('')
     const [ isSending, { setFalse: enableSendBtn, setTrue: disableSendBtn } ] = useBoolean(false)
     const editorRef = useRef<Editor>() as React.MutableRefObject<Editor>
 
@@ -22,24 +28,27 @@ export default function CreateSnippet({ onSent }: CreateSnippetParams) {
         disableSendBtn()
         const contentInput = editorRef.current?.getInstance().getMarkdown()
         const { data } = await axios.post<UploadReturn>('/api/upload', {
+            title,
             content: contentInput,
             owner: wallet.account
         })
         onSent(data)
         enableSendBtn()
-    }, [editorRef])
+    }, [title, onSent, editorRef])
 
-    return <>
+    return <CreateSnippetContainer>
+        <Input placeholder='Title for snippet (Optional)' width='100%' onChange={e => setTitle(e.target.value)} />
         <Editor
-        initialValue=""
+            initialValue={`## Sub Title (If you needed)
+Content`}
         previewStyle="vertical"
         height="600px"
         initialEditType="markdown"
         useCommandShortcut={true}
         ref={editorRef}
         />
-        <div className="exported-md">
-            <Button onClick={() => send()} loading={isSending} disabled={isSending}>Send</Button>
+        <div className="exported-md" style={{ margin: "4px" }}>
+            <Button type="secondary" onClick={() => send()} loading={isSending} disabled={isSending}>Post this snippet</Button>
         </div>
-    </>
+    </CreateSnippetContainer>
 }
