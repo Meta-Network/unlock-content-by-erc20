9cc6c7569cbf91d5f336ee4643bffc0ed8ccfe9c
Frank Wei <frank@frankwei.xyz>
2021-06-09 17:16:40 +0800

    add typing for token list

--- src/typing/index.ts
+++ src/typing/index.ts
@@ -7,7 +7,7 @@ export type Snippet = {
 
 export type RequirementV1 = {
   version: "20210609";
-  type: 'hodl';
+  type: "hodl";
   token: string;
   networkId: number;
   amount: string; // should be good to parsed into BigNumber
@@ -28,4 +28,25 @@ export type EncryptedSnippet = {
   iv: string;
   content: string;
   owner: string;
+};
+
+export type StandardTokenList = {
+  name: string;
+  timestamp: string;
+  version: {
+    major: number;
+    minor: number;
+    patch: number;
+  };
+  tags: Record<string, any>;
+  logoURI: string;
+  keywords: string;
+  tokens: Array<{
+    chainId: number;
+    address: string;
+    name: string;
+    symbol: string;
+    decimals: number;
+    logoURI: string;
+  }>;
 };
