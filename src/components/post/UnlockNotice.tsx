ce7d81e4a01280613249f57a43921cdd0e847b44
Frank Wei <frank@frankwei.xyz>
2021-06-11 16:32:08 +0800

    fix: notify to switch network

--- src/components/post/UnlockNotice.tsx
+++ src/components/post/UnlockNotice.tsx
@@ -1,15 +1,18 @@
-import { Button, Text } from "@geist-ui/react"
+import { Button, Text, Tooltip } from "@geist-ui/react"
 import axios from "axios"
 import { ethers, utils } from "ethers"
 import React, { useCallback, useMemo } from "react"
+import { useRecoilValue } from "recoil"
 import styled from "styled-components"
 import { useWallet } from "use-wallet"
 import { BaseErc20Factory } from "../../blockchain/contracts/BaseErc20Factory"
-import { ChainId } from "../../constant"
+import { ChainId, ChainIdToName } from "../../constant"
 import { getEIP712Profile } from "../../constant/EIP712Domain"
 import { providers } from "../../constant/providers"
+import QuestionCircle from '@geist-ui/react-icons/questionCircle'
 import { useBalance } from "../../hooks/useBalance"
 import { useSigner } from "../../hooks/useSigner"
+import { chainIdState } from "../../stateAtoms/chainId.atom"
 import { Requirement, SnipperForShowing } from "../../typing"
 import { ProfileCard } from "../requirement/ProfileCard"
 
@@ -25,6 +28,7 @@ const UnlockNoticeContainer = styled.div``
 
 export default function UnlockNotice({ onDecrypted, onError, requirement, hash }: UnlockNoticeParams) {
     const wallet = useWallet()
+    const currentConnectedChain = useRecoilValue(chainIdState)
     const token = useMemo(() => {
         const provider = providers[requirement.networkId as ChainId]
         return BaseErc20Factory.connect(requirement.token, provider as ethers.providers.Provider)
@@ -63,14 +67,22 @@ export default function UnlockNotice({ onDecrypted, onError, requirement, hash }
     return <UnlockNoticeContainer>
         <Text>You will need to request a unlock to see this.</Text>
         <ProfileCard currentRequirement={requirement} />
+        
+        <Text>
+            Connected Network: {ChainIdToName[currentConnectedChain] || '❌ Unknown'}
+        </Text>
+
         {wallet.status === 'connected' ?
             <>
-                <Text>👛 { wallet.account }</Text>
+                <Text>👛 {wallet.account}</Text>
                 <Text>Current Balance: {formattedBalance}</Text>
                 { lastUpdated.getTime() !== 0 && <Text> { isEnough(requirement.amount) ? '✅ Is' : '❌ Not' } Qualified to request unlock </Text>}
                 <Button onClick={() => fetchData()}>Verify my HODL & Unlock</Button>
             </>
-            : <Button onClick={() => wallet.connect('injected')}>Connect with MetaMask</Button>
+            :
+            currentConnectedChain === requirement.networkId
+                ? <Button onClick={() => wallet.connect('injected')}>Connect with MetaMask</Button>
+                : `Please switch to ${ChainIdToName[requirement.networkId as ChainId] || requirement.networkId} Network to continue.`
         }
     </UnlockNoticeContainer>
 }
