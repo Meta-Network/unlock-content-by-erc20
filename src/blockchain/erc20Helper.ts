0fb49ae803b278c4b7ed17a7bb1b24a24328c00c
Frank Wei <frank@frankwei.xyz>
2021-06-14 21:17:38 +0800

    fix: support for token that use bytes32 as name

--- src/blockchain/erc20Helper.ts
+++ src/blockchain/erc20Helper.ts
@@ -1,10 +1,11 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { ethers } from "ethers";
+import { ethers, utils } from "ethers";
 import { ChainId } from "../constant";
 import { MULTICALL_NETWORKS } from "../constant/contracts";
 import { providers } from "../constant/providers";
 import { BaseErc20 } from "./contracts/BaseErc20";
 import { Multicall__factory } from "./contracts/MulticallFactory";
+import { ERC20_v0 } from "./contracts/StupidERC20";
 
 export type ERC20Profile = {
   tokenAddress: string;
@@ -21,7 +22,7 @@ export async function getProfileOfERC20(
   holder: string | null
 ): Promise<ERC20Profile> {
   const tokenAddress = token.address;
-  console.info("getProfileOfERC20;:tokenAddress", tokenAddress);
+  console.info("getProfileOfERC20::tokenAddress", tokenAddress);
   const frag = [
     token.interface.encodeFunctionData("name"),
     token.interface.encodeFunctionData("symbol"),
@@ -39,26 +40,62 @@ export async function getProfileOfERC20(
     mCallAddr,
     providers[chainId] as ethers.providers.Provider
   ).callStatic.aggregate(calls);
-  const [name] = token.interface.decodeFunctionResult("name", returnData[0]);
-  const [symbol] = token.interface.decodeFunctionResult(
-    "symbol",
-    returnData[1]
-  );
-  const [decimals] = token.interface.decodeFunctionResult(
-    "decimals",
-    returnData[2]
-  );
-  let balance = BigNumber.from(0);
-  if (returnData[3])
-    [balance] = token.interface.decodeFunctionResult(
-      "balanceOf",
-      returnData[3]
+  try {
+    const [name] = token.interface.decodeFunctionResult("name", returnData[0]);
+    const [symbol] = token.interface.decodeFunctionResult(
+      "symbol",
+      returnData[1]
     );
+    const [decimals] = token.interface.decodeFunctionResult(
+      "decimals",
+      returnData[2]
+    );
+    let balance = BigNumber.from(0);
+    if (returnData[3])
+      [balance] = token.interface.decodeFunctionResult(
+        "balanceOf",
+        returnData[3]
+      );
 
+    return {
+      updatedAtBlock: blockNumber.toNumber(),
+      name,
+      symbol,
+      decimals,
+      balance,
+      tokenAddress,
+    };
+  } catch (error) {
+    console.error("getProfileOfERC20::error", error);
+    return getProfileOfERC20Compatiable(token, chainId, holder);
+  }
+}
+
+/**
+ * only for those who bytes32
+ */
+export async function getProfileOfERC20Compatiable(
+  _token: BaseErc20,
+  chainId: ChainId,
+  holder: string | null
+): Promise<ERC20Profile> {
+  const tokenAddress = _token.address;
+  const token = ERC20_v0.attach(_token.address).connect(_token.provider);
+  console.info("getProfileOfERC20::Compatiable::tokenAddress", tokenAddress);
+  const [blockNumber, name, symbol, decimals] = await Promise.all([
+    (providers[chainId] as ethers.providers.Provider).getBlockNumber(),
+    token.name(),
+    token.symbol(),
+    token.decimals(),
+  ]);
+  let balance = BigNumber.from(0);
+  if (holder) {
+    balance = await token.balanceOf(holder);
+  }
   return {
-    updatedAtBlock: blockNumber.toNumber(),
-    name,
-    symbol,
+    updatedAtBlock: blockNumber,
+    name: utils.parseBytes32String(name),
+    symbol: utils.parseBytes32String(symbol),
     decimals,
     balance,
     tokenAddress,
