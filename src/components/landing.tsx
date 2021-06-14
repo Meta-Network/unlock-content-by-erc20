e3b4440e2b3b2a0612bf7d735a35781756c175ba
Frank Wei <frank@frankwei.xyz>
2021-06-14 15:22:52 +0800

    cleaner page

---
+++ src/components/landing.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components"
+import React, { } from "react";
+import PlusSquare from '@geist-ui/react-icons/plusSquare'
+import QuestionCircle from '@geist-ui/react-icons/questionCircle'
+import { Button, Text } from "@geist-ui/react";
+import Link from "next/link";
+
+const LandingPageContainer = styled.div``
+const HeaderPart = styled.div``
+const FeaturesDisplays = styled.div``
+const ActionBtns = styled.div`
+button {
+    margin-right: 1rem;
+}`
+
+
+export default function LandingPage() {
+    return <LandingPageContainer>
+        <HeaderPart>
+            <Text h1>
+                (Un)Lock with Hodl
+            </Text>
+            <Text h4 type="secondary">
+                Encrypt your article / snippet and set a Proof of Hodl (POH) requirement for content unlock!
+            </Text>
+            <Text p>We currently supported some tokens for Ethereum Mainnet and BSC Mainnet.</Text>
+            <ActionBtns>
+                <Link href="/create">
+                    <Button type="secondary" icon={<PlusSquare />}>Create One</Button>
+                </Link>
+                <Link href="/faq">
+                    <Button auto icon={<QuestionCircle />}>FAQ</Button>
+                </Link>
+                </ActionBtns>
+        </HeaderPart>
+        <FeaturesDisplays>
+        </FeaturesDisplays>
+    </LandingPageContainer>
+}
---
+++ src/pages/faq.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+import React, { } from "react";
+import QuestionCircle from '@geist-ui/react-icons/questionCircle'
+import { Button, Text } from "@geist-ui/react";
+
+const Container = styled.div``
+
+export default function FAQPage() {
+    return <Container>
+        <p>T.B.C</p>
+    </Container>
+}
