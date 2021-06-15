4d4f5cc0fac68ff966f88abdd11b4c3d83a22963
Frank Wei <frank@frankwei.xyz>
2021-06-15 17:56:33 +0800

    refactor: popup the list and good to search

--- src/components/TokenSelector.tsx
+++ src/components/TokenSelector.tsx
@@ -5,11 +5,14 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { StandardTokenList, StandardTokenProfile, UploadReturn } from "../typing";
 import { useWallet } from "use-wallet";
-import { Button, Select } from "@geist-ui/react";
+import { Button, Grid, Input, Modal, Text, useModal, User } from "@geist-ui/react";
 import { useBoolean } from "ahooks";
 import styled from "styled-components";
 import useSWR from "swr";
 import { axiosSWRFetcher } from "../utils";
+import { utils } from "ethers";
+import { ChainId, ChainIdToName } from "../constant";
+import { useERC20 } from "../hooks/useERC20";
 
 type TokenSelectorParams = {
     selectedChainId: number;
@@ -17,11 +20,14 @@ type TokenSelectorParams = {
     onSelected: (obj: StandardTokenProfile) => void
 }
 
-const PageContainer = styled.div`
-max-width: 100%;
-`
 
 export default function TokenSelector({ onSelected, selectedChainId }: TokenSelectorParams) {
+    const { visible, setVisible, bindings } = useModal()
+    const [searchInput, setSearchInput] = useState('')
+
+    const isContractAddress = useMemo(() => utils.isAddress(searchInput), [searchInput]);
+    const { tokenProfile } = useERC20(isContractAddress ? searchInput : null, selectedChainId)
+
     const tokenListURI = useMemo(() => {
         switch (selectedChainId) {
             case 1: return 'https://gateway.ipfs.io/ipns/tokens.uniswap.org';
@@ -34,26 +40,66 @@ export default function TokenSelector({ onSelected, selectedChainId }: TokenSele
 
     const tokensOnCurrentChain = useMemo(() => {
         if (!data) return []
-        return (data as StandardTokenList).tokens.filter(t => t.chainId === selectedChainId)
-    }, [data, selectedChainId])
 
-    const onTokenSelected = useCallback((address: string) => {
-        const targetToken = tokensOnCurrentChain.find((val) => val.address === address);
-        if (targetToken) onSelected(targetToken)
-    }, [tokensOnCurrentChain, onSelected])
+        const onlyThisChain = (data as StandardTokenList).tokens.filter(t => t.chainId === selectedChainId)
+
+        if (searchInput === '') return onlyThisChain
+        else if (isContractAddress) {
+            // @todo: should be the input token only
+            const findInList = onlyThisChain.find((t) => utils.getAddress(t.address) === utils.getAddress(searchInput))
+            if (findInList) return [findInList]
+            if (!tokenProfile) return []
+            return [{
+                chainId: selectedChainId,
+                address: searchInput,
+                logoURI: 'https://raw.githubusercontent.com/ant-design/ant-design-icons/master/packages/icons-svg/svg/outlined/question-circle.svg',
+                name: tokenProfile.name,
+                symbol: tokenProfile.symbol,
+            }] as StandardTokenProfile[]
+        } else {
+            // is search on name then, go
+            return onlyThisChain.filter(t => t.name.includes(searchInput) || t.symbol.includes(searchInput))
+        }
+
+    }, [data, selectedChainId, searchInput, isContractAddress, tokenProfile])
+
+    // const onTokenSelected = useCallback((address: string) => {
+    //     const targetToken = tokensOnCurrentChain.find((val) => val.address === address);
+    //     if (targetToken) onSelected(targetToken)
+    // }, [tokensOnCurrentChain, onSelected])
 
     if (!data) return <p>Loading Token List...</p>
 
-    return <PageContainer>
-        <Select placeholder="Select Token" onChange={v => onTokenSelected(v as string)}>
-            {
-                tokensOnCurrentChain.map(token =>
-                    <Select.Option
-                        value={token.address}
-                        key={token.address}
-                    >{token.name} - {token.symbol}</Select.Option>
-                )
-            }
-        </Select>
-    </PageContainer>
+    return (
+        <>
+        <Button auto onClick={() => setVisible(true)}>Select Token</Button>
+        <Modal {...bindings}>
+            <Modal.Title>Select Token</Modal.Title>
+                <Modal.Subtitle>
+                    Current List: xxxxxxx Change List<br />
+                    👛 Current Chain: {ChainIdToName[selectedChainId as ChainId] || 'Not Supported'}
+                </Modal.Subtitle>
+            <Modal.Content>
+                <Input placeholder="Search by name or paste address..."
+                    onChange={(e) => setSearchInput(e.target.value)}
+                        width="100%" />
+                    <Grid.Container gap={2} justify="center" style={{ marginTop: '1rem' }}>
+                        {
+                            tokensOnCurrentChain.map(token =>
+                                <Grid xs>
+                                    <User src={token.logoURI} name={token.symbol} onClick={() => {
+                                        onSelected(token)
+                                        setVisible(false)
+                                    }}>
+                                        <Text>{ token.address.slice(0,6) }...{ token.address.slice(-4) }</Text>
+                                    </User>
+                                </Grid>
+                            )
+                        }
+                    </Grid.Container>
+            </Modal.Content>
+            <Modal.Action passive onClick={() => setVisible(false)}>Close</Modal.Action>
+        </Modal>
+        </>
+    )
 }
