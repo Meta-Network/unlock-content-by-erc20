365b10b71a64b7d5b219f4015488ee9d9a80c0e9
Frank Wei <frank@frankwei.xyz>
2021-06-17 15:33:03 +0800

    fix: workaround for stupid metamask bug

--- src/constant/EIP712Domain.ts
+++ src/constant/EIP712Domain.ts
@@ -3,7 +3,7 @@ import { ZERO_ADDRESS } from ".";
 export const getEIP712Profile = (chainId: number) => ({
   name: "Hodl To Unlock",
   version: "1",
-  chainId,
+  // chainId,
   // verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
-  verifyingContract: ZERO_ADDRESS,
+  // verifyingContract: ZERO_ADDRESS,
 });
